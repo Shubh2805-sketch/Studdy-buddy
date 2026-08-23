3e4aa1a9053176418e4fb8a728b06b931f3ab74a
Shubh2805-sketch <shubhangambinju30@gmail.com>
2026-08-23 10:58:40 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const StudyBuddyApp());
+}
+
+class StudyBuddyApp extends StatelessWidget {
+  const StudyBuddyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'StudyBuddy',
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('StudyBuddy'),
+      ),
+      body: const Center(
+        child: Text(
+          'StudyBuddy is working!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
